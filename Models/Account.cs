d3853f5fde0430e28e8c2eedcd31dd55228e5ffc
Jonas Martin Smidt <jos005@edu.zealand.dk>
2024-09-26 10:25:01 +0200

    Acoount model

--- Models/Account.cs
+++ Models/Account.cs
@@ -2,6 +2,9 @@
 {
     public class Account
     {
-        //test
+        
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Balance { get; set; }
     }
 }
