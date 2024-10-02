534eddd7f922e892f24f3ae77d9dc7993c94a0a1
Daniyal <Daniyal1604@gmail.com>
2024-10-02 10:38:01 +0200

    new

--- Pages/Withdraw.cshtml.cs
+++ Pages/Withdraw.cshtml.cs
@@ -1,11 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using SpankBank1.Interface;
+using SpankBank1.Models;
 
 namespace SpankBank1.Pages
 {
     public class WithdrawModel : PageModel
     {
+        public Account Account { get; set; }
+        public decimal WithdrawAmount { get; set; }
+
         private readonly IAccountService _bankService;
 
         public WithdrawModel(IAccountService bankService)
@@ -16,21 +20,29 @@ namespace SpankBank1.Pages
         [BindProperty]
         public decimal Balance { get; set; }
 
-        public void OnGet()
+        //public void OnGet()
+        //{
+
+        //    Balance = 0; // initial balance
+
+        //}
+
+        public IActionResult OnGet(int id)
         {
-
-            Balance = 0; // initial balance
-
+            Account = _bankService.GetAccountById(id);
+            return Page();
         }
+
+
 
         public IActionResult OnPost(int id, decimal amount)
         {
             if (ModelState.IsValid)
             {
                 _bankService.WithdrawAccount(id, amount);
+                Account = _bankService.GetAccountById(id);
             }
             return Page();
         }
-
     }
 }
